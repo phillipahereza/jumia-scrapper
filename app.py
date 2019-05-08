660083f5fbe0a6abb51e2c2e239c0c6bd325ff08
Phillip Ahereza <pahereza@gmail.com>
2019-05-08 18:33:17 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from zappa.asynchronous import task
+
+from lib.scraper import start_scraping
+
+app = Flask(__name__)
+
+
+@task
+def scrape():
+    start_scraping()
+
+
+@app.route('/scrape')
+def index():
+    scrape()
+    return "Started Scraping", 200
+
+
+# We only need this for local development.
+if __name__ == '__main__':
+    app.run()
